6e1127188c41df66f0b317adba1f94eddc7b6e2c
ENOCH DANIEL <dynablaze@gmail.com>
2018-05-28 09:15:36 +0100

    added new files
    upgraded contact us,index faq

--- ejscripts.js
+++ ejscripts.js
@@ -6,15 +6,12 @@
  * Created by Nas on 3/28/2018.
 
  */
-(document).ready(function() {
-    if ($('#contactform').length > 0 ) {
-        contactScript('forcontact');
-    }
-});
 
-// Initialize Firebase
-function contactScript(value) {
-    let config = {
+
+
+
+
+    var config = {
         apiKey: "AIzaSyBmL2ysOMStkcwSnOYvylKcSh24HfnU2Ps",
         authDomain: "spackbit-contact.firebaseapp.com",
         databaseURL: "https://spackbit-contact.firebaseio.com",
@@ -24,90 +21,52 @@ function contactScript(value) {
     };
     firebase.initializeApp(config);
 
-    let b = firebase.database().ref("messages");
-    $("#contactform").submit(function(config) { $(this), console.log("Submit to Firebase");
-    let c = $("#username").val(),
-        d = $("#useremail").val(),
-        f = { name: c, email: d};
-    return b.push(f).then(function(config) {
-        $(".sucess").css("display", "block"),
-            $(".formsuccess").css("display", "none") }), !1 })
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactform').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+    e.preventDefault();
+
+    //Get value
+    var name = getInputVal('username');
+    var email = getInputVal('useremail');
+    var phone = getInputVal('phoneno');
+    var message = getInputVal('brief');
+
+    // Save message
+    saveMessage(name, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+    },3000);
+
+    // Clear form
+    document.getElementById('contactform').reset();
 }
 
-
-function init(){
-    imgobj =document.getElementById('mac');
-    imgobj.style.position='relative';
-    imgobj.style.left='opx';
-
-}
-function moveright (){
-    imgobj.style.left = parseInt(imgobj.style.left)+10+'px';
-    animate = setTimeout(moveright,100);
-
-}
-function stop() {
-    clearTimeout(animate);
-    imgobj.style.left = '0px';
+// Function to get form value
+function getInputVal(id){
+    return document.getElementById(id).value;
 }
 
-window.onload=init;
-
-function homein(){
-    let but1 =document.getElementById('home');
-    but1.style.backgroundColor="blue";
-    but1.style.fontSize='5pt';
+// Save message to firebase
+function saveMessage(name,  email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        phone: phone,
+        message: message
+    });
 }
-function homeout(){
-    let but1 =document.getElementById('home');
-    but1.style.backgroundColor="black";
-    but1.style.fontSize='10pt';
-}
-
-function aboutin(){
-    let but1 =document.getElementById('about');
-    but1.style.backgroundColor="red";
-    but1.style.fontSize='5pt';
-}
-function aboutout(){
-    let but1 =document.getElementById('about');
-    but1.style.backgroundColor="black";
-    but1.style.fontSize='10pt';
-}
-
-function conin(){
-    let but1 =document.getElementById('contact');
-    but1.style.backgroundColor="pink";
-    but1.style.fontSize='5pt';
-}
-function conout(){
-    let but1 =document.getElementById('contact');
-    but1.style.backgroundColor="black";
-    but1.style.fontSize='10pt';
-}
-
-function view1() {
-    let element = document.getElementById("viewans1");
-    element.classList.toggle("viewans1");
-}
-
-function view2() {
-    let element = document.getElementById("viewans1");
-    element.classList.toggle("viewans2");
-}
-
-function view3() {
-    let element = document.getElementById("viewans1");
-    element.classList.toggle("viewans3");
-}
-
-function view4() {
-    let element = document.getElementById("viewans1");
-    element.classList.toggle("viewans4");
-}
-
-
-
 
 $(document).ready(function(){
     $("#hide").click(function(){
